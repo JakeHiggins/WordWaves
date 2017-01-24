2f47bf7229efb9d0661f3d4560fe6b217ee54a7e
Jake Higgins <jth7036@gmail.com>
2017-01-23 20:25:09 -0500

    Added parseDictionary.py to solution

--- WordWaves/Phrases/parseDictionary.py
+++ WordWaves/Phrases/parseDictionary.py
@@ -3,8 +3,11 @@ import sys
 def main():
   try:
 	file = open(sys.argv[1], "r")
+	words = []
 	for line in file:
-		print(line, end='')
+		words.append(line)
+
+	print(len(words))
 
   except:
     print("Missing arguments. Expects: python parseDictionary.py inputDictionary.txt minLength maxLength outDir")
